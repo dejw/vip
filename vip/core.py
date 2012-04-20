966e6b433b36f9cc5a4910441716d670302124dc
Dawid Fatyga <dawid.fatyga@gmail.com>
2012-04-21 01:52:08 +0200

    Fixed .vip finder tests.

--- vip/core.py
+++ vip/core.py
@@ -11,21 +11,22 @@ def find_vip_directory(start="."):
     """ Finds the `.vip` directory up in the directory tree.
 
     Args:
-      start: str directory name from where the search is started
+        start: str directory name from where the search is started
 
     Returns:
         An absolute path to `.vip` directory.
 
     Raises:
-        VipError when no `.vip` directory has been found to the root.
+        VipError: when no `.vip` directory has been found to the root.
     """
-    directory = path.abspath(start)
+    directory, head = path.abspath(start), None
 
-    while directory != "/":
+    while directory != "/" or head:
         vip_directory = path.join(directory, ".vip")
+
         if path.exists(vip_directory) and path.isdir(vip_directory):
             return vip_directory
 
-        directory, _ = path.split(directory)
+        directory, head = path.split(directory)
 
     raise VipError("not a virtualenv (or any of the parent directories)")
--- vip/test/test_core.py
+++ vip/test/test_core.py
@@ -17,10 +17,17 @@ class TestVipDirectoryFinder(unittest.TestCase):
         self.assertEqual(path.abspath(path.join(start, ".vip")), directory)
 
     def test_should_skip_vip_which_is_no_directory(self):
-        start = path.join(path.abspath(__file__), "fixtures", "test_fail")
+        root = path.join(path.dirname(__file__), "fixtures", "test2")
+
+        directory = core.find_vip_directory(start=path.join(root, "with_plain_file"))
+
+        self.assertEqual(path.join(root, ".vip"), directory)
+
+    def test_should_raise_VipError_when_no_vip_is_found(self):
+        root = path.join(path.dirname(__file__), "fixtures", "test3")
 
         with self.assertRaisesRegexp(core.VipError, "not a virtualenv"):
-            core.find_vip_directory(start=start)
+            core.find_vip_directory(start=root)
 
 
 if __name__ == "__main__":
