6ed0dc554d257a99e81366447b35c1615aa002c0
Charles Grunwald (Juntalis) <charles@grunwald.me>
2012-11-14 12:06:26 -0800

    Added tests for Windows.

--- vip/test/test_core.py
+++ vip/test/test_core.py
@@ -41,66 +41,14 @@ class TestVipDirectoryFinder(unittest.TestCase):
 
         directory = core.find_vip_directory(start=path.join(root,
                                                             "with_plain_file"))
-
-        self.assertEqual(path.join(root, ".vip"), directory)
+        expected = path.abspath(path.join(root, ".vip"))
+        self.assertEqual(expected, directory)
 
     def test_should_raise_VipError_when_no_vip_is_found(self):
         root = path.join(path.dirname(__file__), "fixtures", "test3")
 
         with self.assertRaisesRegexp(core.VipError, "not a virtualenv"):
             core.find_vip_directory(start=root)
-
-
-class TestCommandExecution(unittest.TestCase):
-
-    def setUp(self):
-        self.mox = mox.Mox()
-
-        self.mox.StubOutWithMock(subprocess, "Popen")
-
-        self.popen_mock = self.mox.CreateMockAnything("popen")
-        self.popen_mock.stdin = self.mox.CreateMockAnything("stdin")
-
-        # Assert that stdin is closed
-        self.popen_mock.stdin.close()
-
-        dirname = path.dirname(__file__)
-        self.vip_dir = path.join(dirname, "fixtures", "test1", ".vip")
-
-        (subprocess
-            .Popen([EndsWith("test1/.vip/bin/command"), "-arg", "123"],
-                   stdout=mox.IgnoreArg(), stderr=mox.IgnoreArg(),
-                   stdin=subprocess.PIPE)
-            .AndReturn(self.popen_mock))
-
-    def tearDown(self):
-        self.mox.ResetAll()
-        self.mox.UnsetStubs()
-
-    def test_should_raise_VipError_when_command_is_not_found(self):
-
-        with self.assertRaisesRegexp(core.VipError, "not found"):
-            core.execute_virtualenv_command("missing/.vip", "command", [])
-
-    def test_should_call_command(self):
-        self.popen_mock.communicate()
-        self.mox.ReplayAll()
-
-        core.execute_virtualenv_command(self.vip_dir, "command",
-                                        ["-arg", "123"])
-
-        self.mox.VerifyAll()
-
-    def test_should_raise_VipError_when_CalledProcessError_is_found(self):
-        (self.popen_mock.communicate()
-            .AndRaise(subprocess.CalledProcessError(1, "error")))
-        self.mox.ReplayAll()
-
-        with self.assertRaises(core.VipError):
-            core.execute_virtualenv_command(self.vip_dir, "command",
-                                            ["-arg", "123"])
-
-        self.mox.VerifyAll()
 
 
 if osname == 'nt':
@@ -155,3 +103,106 @@ if osname == 'nt':
 
             with self.assertRaisesRegexp(core.VipError, "not a virtualenv"):
                 core.find_vip_directory(start=root)
+
+
+    class TestWindowsCommandExecution(unittest.TestCase):
+
+        def setUp(self):
+            self.mox = mox.Mox()
+
+            self.mox.StubOutWithMock(subprocess, "Popen")
+
+            self.popen_mock = self.mox.CreateMockAnything("popen")
+            self.popen_mock.stdin = self.mox.CreateMockAnything("stdin")
+
+            # Assert that stdin is closed
+            self.popen_mock.stdin.close()
+
+            dirname = path.dirname(__file__)
+            self.vip_dir = path.join(dirname, "fixtures", "test4", ".vip")
+
+            (subprocess
+                .Popen([EndsWith("test4\\.vip\\Scripts\\command"), "-arg", "123"],
+                       stdout=mox.IgnoreArg(), stderr=mox.IgnoreArg(),
+                       stdin=subprocess.PIPE)
+                .AndReturn(self.popen_mock))
+
+        def tearDown(self):
+            self.mox.ResetAll()
+            self.mox.UnsetStubs()
+
+        def test_should_raise_VipError_when_command_is_not_found(self):
+
+            with self.assertRaisesRegexp(core.VipError, "not found"):
+                core.execute_virtualenv_command("missing\\.vip", "command", [])
+
+        def test_should_call_command(self):
+            self.popen_mock.communicate()
+            self.mox.ReplayAll()
+
+            core.execute_virtualenv_command(self.vip_dir, "command",
+                                            ["-arg", "123"])
+
+            self.mox.VerifyAll()
+
+        def test_should_raise_VipError_when_CalledProcessError_is_found(self):
+            (self.popen_mock.communicate()
+                .AndRaise(subprocess.CalledProcessError(1, "error")))
+            self.mox.ReplayAll()
+
+            with self.assertRaises(core.VipError):
+                core.execute_virtualenv_command(self.vip_dir, "command",
+                                                ["-arg", "123"])
+
+            self.mox.VerifyAll()
+else:
+    class TestCommandExecution(unittest.TestCase):
+
+        def setUp(self):
+            self.mox = mox.Mox()
+
+            self.mox.StubOutWithMock(subprocess, "Popen")
+
+            self.popen_mock = self.mox.CreateMockAnything("popen")
+            self.popen_mock.stdin = self.mox.CreateMockAnything("stdin")
+
+            # Assert that stdin is closed
+            self.popen_mock.stdin.close()
+
+            dirname = path.dirname(__file__)
+            self.vip_dir = path.join(dirname, "fixtures", "test1", ".vip")
+
+            (subprocess
+                .Popen([EndsWith("test1/.vip/bin/command"), "-arg", "123"],
+                       stdout=mox.IgnoreArg(), stderr=mox.IgnoreArg(),
+                       stdin=subprocess.PIPE)
+                .AndReturn(self.popen_mock))
+
+        def tearDown(self):
+            self.mox.ResetAll()
+            self.mox.UnsetStubs()
+
+        def test_should_raise_VipError_when_command_is_not_found(self):
+
+            with self.assertRaisesRegexp(core.VipError, "not found"):
+                core.execute_virtualenv_command("missing/.vip", "command", [])
+
+        def test_should_call_command(self):
+            self.popen_mock.communicate()
+            self.mox.ReplayAll()
+
+            core.execute_virtualenv_command(self.vip_dir, "command",
+                                            ["-arg", "123"])
+
+            self.mox.VerifyAll()
+
+        def test_should_raise_VipError_when_CalledProcessError_is_found(self):
+            (self.popen_mock.communicate()
+                .AndRaise(subprocess.CalledProcessError(1, "error")))
+            self.mox.ReplayAll()
+
+            with self.assertRaises(core.VipError):
+                core.execute_virtualenv_command(self.vip_dir, "command",
+                                                ["-arg", "123"])
+
+            self.mox.VerifyAll()
