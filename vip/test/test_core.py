e7b8c30d0831433e7df6fe2fd0dc29f5aa5761e0
Charles Grunwald (Juntalis) <charles@grunwald.me>
2012-11-14 17:52:53 -0800

    Remove code redundancies.

--- vip/test/test_core.py
+++ vip/test/test_core.py
@@ -9,7 +9,7 @@ except ImportError:
     import unittest
 
 
-from os import path, name as osname
+from os import path
 
 from vip import core
 
@@ -28,8 +28,15 @@ class EndsWith(mox.Comparator):
 
 class TestVipDirectoryFinder(unittest.TestCase):
 
+    def setUp(self):
+        # On Windows, dirname(__file__) would return a path to the folder
+        # that uses the alternative seperators. (C:/path/to/dir) For the
+        # purpose of this test, though, we want to use the OS's native file
+        # paths.
+        self.root = path.normpath(path.dirname(__file__))
+
     def test_should_return_absolute_path_to_vip_directory(self):
-        start = path.join(path.dirname(__file__), "fixtures", "test1", "..",
+        start = path.join(self.root, "fixtures", "test1", "..",
                           "test1")
 
         directory = core.find_vip_directory(start=start)
@@ -37,7 +44,7 @@ class TestVipDirectoryFinder(unittest.TestCase):
         self.assertEqual(path.abspath(path.join(start, ".vip")), directory)
 
     def test_should_skip_vip_which_is_no_directory(self):
-        root = path.join(path.dirname(__file__), "fixtures", "test2")
+        root = path.join(self.root, "fixtures", "test2")
 
         directory = core.find_vip_directory(start=path.join(root,
                                                             "with_plain_file"))
@@ -45,121 +52,23 @@ class TestVipDirectoryFinder(unittest.TestCase):
         self.assertEqual(expected, directory)
 
     def test_should_raise_VipError_when_no_vip_is_found(self):
-        root = path.join(path.dirname(__file__), "fixtures", "test3")
+        root = path.join(self.root, "fixtures", "test3")
 
         with self.assertRaisesRegexp(core.VipError, "not a virtualenv"):
             core.find_vip_directory(start=root)
 
 
 @unittest.skipUnless(core.is_win, "Windows-specific test")
-class TestWindowsVipDirectoryFinder(unittest.TestCase):
+class TestWindowsVipDirectoryFinder(TestVipDirectoryFinder):
 
     def setUp(self):
-        drive, folderpath = path.splitdrive(
-            path.abspath(path.dirname(__file__)))
-
-        # The test root folder with a backslash following the drive letter.
-        # In normal circumstances, C:\
-        self.rootbs = path.join(drive, '\\' + folderpath[1:])
-        # The test root folder with a slash following the drive letter.
-        # In normal circumstances, C:/
-        self.rootfs = path.join(drive, '/' + folderpath[1:])
-
-    def test_should_return_absolute_path_to_vip_directory_backslash(self):
-        start = path.join(self.rootbs, "fixtures", "test1", "..", "test1")
-
-        directory = core.find_vip_directory(start=start)
-
-        self.assertEqual(path.abspath(path.join(start, ".vip")), directory)
-
-    def test_should_return_absolute_path_to_vip_directory_slash(self):
-        start = path.join(self.rootfs, "fixtures", "test1", "..", "test1")
-
-        directory = core.find_vip_directory(start=start)
-
-        self.assertEqual(path.abspath(path.join(start, ".vip")), directory)
-
-    def test_should_skip_vip_which_is_no_directory_backslash(self):
-        root = path.join(self.rootbs, "fixtures", "test2")
-        start = path.join(root, "with_plain_file")
-        directory = core.find_vip_directory(start=start)
-        vip_folder = path.abspath(path.join(root, ".vip"))
-        self.assertEqual(vip_folder, directory)
-
-    def test_should_skip_vip_which_is_no_directory_slash(self):
-        root = path.join(self.rootfs, "fixtures", "test2")
-        start = path.join(root, "with_plain_file")
-        directory = core.find_vip_directory(start=start)
-        vip_folder = path.abspath(path.join(root, ".vip"))
-        self.assertEqual(vip_folder, directory)
-
-    def test_should_raise_VipError_when_no_vip_is_found_backslash(self):
-        root = path.join(self.rootbs, "fixtures", "test3")
-
-        with self.assertRaisesRegexp(core.VipError, "not a virtualenv"):
-            core.find_vip_directory(start=root)
-
-    def test_should_raise_VipError_when_no_vip_is_found_slash(self):
-        root = path.join(self.rootfs, "fixtures", "test3")
-
-        with self.assertRaisesRegexp(core.VipError, "not a virtualenv"):
-            core.find_vip_directory(start=root)
+        # Because no Windows path can contain the \ character,
+        # replacing all \'s characters with / will only change
+        # the separator
+        super(TestWindowsVipDirectoryFinder, self).setUp()
+        self.root = self.root.replace(path.sep, path.altsep)
 
 
-@unittest.skipUnless(core.is_win, "Windows-specific test")
-class TestWindowsCommandExecution(unittest.TestCase):
-
-    def setUp(self):
-        self.mox = mox.Mox()
-
-        self.mox.StubOutWithMock(subprocess, "Popen")
-
-        self.popen_mock = self.mox.CreateMockAnything("popen")
-        self.popen_mock.stdin = self.mox.CreateMockAnything("stdin")
-
-        # Assert that stdin is closed
-        self.popen_mock.stdin.close()
-
-        dirname = path.dirname(__file__)
-        self.vip_dir = path.join(dirname, "fixtures", "test4", ".vip")
-        command = "test4\\.vip\\Scripts\\command"
-        (subprocess
-            .Popen([EndsWith(command), "-arg", "123"],
-                   stdout=mox.IgnoreArg(), stderr=mox.IgnoreArg(),
-                   stdin=subprocess.PIPE)
-            .AndReturn(self.popen_mock))
-
-    def tearDown(self):
-        self.mox.ResetAll()
-        self.mox.UnsetStubs()
-
-    def test_should_raise_VipError_when_command_is_not_found(self):
-
-        with self.assertRaisesRegexp(core.VipError, "not found"):
-            core.execute_virtualenv_command("missing\\.vip", "command", [])
-
-    def test_should_call_command(self):
-        self.popen_mock.communicate()
-        self.mox.ReplayAll()
-
-        core.execute_virtualenv_command(self.vip_dir, "command",
-                                        ["-arg", "123"])
-
-        self.mox.VerifyAll()
-
-    def test_should_raise_VipError_when_CalledProcessError_is_found(self):
-        (self.popen_mock.communicate()
-            .AndRaise(subprocess.CalledProcessError(1, "error")))
-        self.mox.ReplayAll()
-
-        with self.assertRaises(core.VipError):
-            core.execute_virtualenv_command(self.vip_dir, "command",
-                                            ["-arg", "123"])
-
-        self.mox.VerifyAll()
-
-
-@unittest.skipIf(core.is_win, "POSIX-specific test")
 class TestCommandExecution(unittest.TestCase):
 
     def setUp(self):
@@ -173,11 +82,18 @@ class TestCommandExecution(unittest.TestCase):
         # Assert that stdin is closed
         self.popen_mock.stdin.close()
 
-        dirname = path.dirname(__file__)
-        self.vip_dir = path.join(dirname, "fixtures", "test1", ".vip")
+        # OS-specific stuff.
+        casedir = "test4" if core.is_win else "test1"
+        bindir = "Scripts" if core.is_win else "bin"
 
+        # See TestVipDirectoryFinder for the reason behind the normpath call.
+        dirname = path.normpath(path.dirname(__file__))
+        vip_dir_parts = [casedir, ".vip"]
+        self.vip_dir = path.join(dirname, "fixtures", *vip_dir_parts)
+
+        command = path.sep.join(vip_dir_parts + [bindir, "command"])
         (subprocess
-            .Popen([EndsWith("test1/.vip/bin/command"), "-arg", "123"],
+            .Popen([EndsWith(command), "-arg", "123"],
                    stdout=mox.IgnoreArg(), stderr=mox.IgnoreArg(),
                    stdin=subprocess.PIPE)
             .AndReturn(self.popen_mock))
@@ -187,9 +103,9 @@ class TestCommandExecution(unittest.TestCase):
         self.mox.UnsetStubs()
 
     def test_should_raise_VipError_when_command_is_not_found(self):
-
+        missing = path.sep.join(["missing", ".vip"])
         with self.assertRaisesRegexp(core.VipError, "not found"):
-            core.execute_virtualenv_command("missing/.vip", "command", [])
+            core.execute_virtualenv_command(missing, "command", [])
 
     def test_should_call_command(self):
         self.popen_mock.communicate()
