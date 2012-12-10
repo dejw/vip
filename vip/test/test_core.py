fc1a5dadcf30432f20356c7c2e3706a478200420
Dawid Fatyga <dawid.fatyga@gmail.com>
2012-12-10 01:15:12 +0100

    Added get_requirements_filenames method.

--- vip/test/test_core.py
+++ vip/test/test_core.py
@@ -196,3 +196,25 @@ class TestWindowsFindExecutable(unittest.TestCase):
 
         with self.assertRaisesRegexp(core.VipError, "not found"):
             core.find_windows_executable(base_exe)
+
+
+class TestGetRequirementsFilenames(unittest.TestCase):
+
+    def test_empty(self):
+        names = list(core.get_requirements_filenames('', ''))
+
+        self.assertEqual(['requirements.txt'], names)
+
+    def test_prefix_and_version(self):
+        names = list(core.get_requirements_filenames('devel', (2, 7, 3)))
+
+        self.assertEqual(map(str.strip, """
+              requirements.txt
+              requirements-2.txt
+              requirements-27.txt
+              requirements-273.txt
+        devel-requirements.txt
+        devel-requirements-2.txt
+        devel-requirements-27.txt
+        devel-requirements-273.txt
+        """.strip().splitlines()), names)
